fb2a9e8aba1584e41362b2c4adee38e4f4d18a74
François Michonneau <francois.michonneau@gmail.com>
2018-07-05 12:20:35 -0400

    figure out directory structure

--- amy_add.sh
+++ amy_add.sh
@@ -1,10 +1,13 @@
+ls .. -la;
+
 cd .. &&
-    git clone git@github.com:carpentries/amy-feeds.git amy-feeds &&
+    git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
     cd amy-feeds &&
     make amy &&
     cp _data/dc_amy.yml ../datacarpentry.github.io/_data/amy.yml
 
 cd ../datacarpentry.github.io &&
+    git remote -v &&
     git checkout master &&
     git pull origin master &&
     git add _data/amy.yml &&
