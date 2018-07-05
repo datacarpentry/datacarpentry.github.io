27b379b160ad1cd3f61d3845da7c7ce80120ab26
François Michonneau <francois.michonneau@gmail.com>
2018-07-05 14:55:07 -0400

    rename remote

--- amy_add.sh
+++ amy_add.sh
@@ -5,7 +5,7 @@ cd .. &&
     cp _data/dc_amy.yml ../datacarpentry.github.io/_data/amy.yml
 
 cd ../datacarpentry.github.io
-git remote set-url origin git@github.com:datacarpentry/datacarpentry.github.io.git
+git remote add deploy git@github.com:datacarpentry/datacarpentry.github.io.git
 
 git config user.name "François Michonneau"
 git config user.email "francois.michonneau@gmail.com"
@@ -13,4 +13,4 @@ git config user.email "francois.michonneau@gmail.com"
 git checkout master
 git add _data/amy.yml
 git commit -m "[ci skip] updating DC workshop data feed"
-git push origin master
+git push deploy master
