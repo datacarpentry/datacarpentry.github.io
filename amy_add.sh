935c594d9ad8a5092407e166a766b56e698de402
François Michonneau <francois.michonneau@gmail.com>
2018-07-05 17:28:03 -0400

    try keys

--- amy_add.sh
+++ amy_add.sh
@@ -5,10 +5,20 @@ cd .. &&
     cp _data/dc_amy.yml ../datacarpentry.github.io/_data/amy.yml
 
 cd ../datacarpentry.github.io
-git remote add deploy https://"$GITHUB_PAT"@github.com/datacarpentry/datacarpentry.github.io.git > /dev/null 2>&1
+git remote add deploy git@github.com:datacarpentry/datacarpentry.github.io.git
 
 git config user.name "François Michonneau"
 git config user.email "francois.michonneau@gmail.com"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
 
 git checkout master
 git add _data/amy.yml
