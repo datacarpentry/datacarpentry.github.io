c389867fc177f930b490ba2d2e2c5d2bc62885e4
François Michonneau <francois.michonneau@gmail.com>
2018-07-06 10:46:30 -0400

    redo travis

--- amy_add.sh
+++ amy_add.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
+
+REPO_ORG=datacarpentry
+REPO_NAME=datacarpentry.github.io
+GITHUB_PAT_USER=fmichonneau # user who generated the GITHUB PAT used here
+
 cd .. &&
+    rm -rf amy-feeds/ &&
     git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
     cd amy-feeds &&
     make amy &&
-    cp _data/dc_amy.yml ../datacarpentry.github.io/_data/amy.yml
+    cp _data/dc_amy.yml ../"$REPO_NAME"/_data/amy.yml
 
-cd ../datacarpentry.github.io
-git remote add deploy git@github.com:datacarpentry/datacarpentry.github.io.git
+cd ../"$REPO_NAME"  || exit
+
+git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout master
+git add _data/amy.yml
+git commit -m "[ci skip] update workshop data"
+git push deploy master
