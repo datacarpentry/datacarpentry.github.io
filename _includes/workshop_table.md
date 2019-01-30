c030c93d917b7422d4498ff9bd5ca7f492bc0eb6
François Michonneau <francois.michonneau@gmail.com>
2019-01-30 06:07:47 -0500

    use new data source for list of workshops

--- _includes/workshop_table.md
+++ _includes/workshop_table.md
@@ -1,5 +1,5 @@
 <table class="table table-striped workshops">
-{% for w in site.data.amy  %}
+{% for w in site.data.DC_upcoming_workshops_plain  %}
 	<tr>
 	<td>
 		<img src="{{site.url}}/assets/img/flags/{{site.flag_size}}/{{w.country | downcase}}.png" title="{{w.country | replace: '-', ' '}}" alt="{{w.country | replace: '-', ' ' | downcase}}" />
--- amy_add.sh
+++ amy_add.sh
@@ -8,14 +8,14 @@ cd .. &&
     rm -rf amy-feeds/ &&
     git clone https://github.com/carpentries/amy-feeds.git amy-feeds &&
     cd amy-feeds &&
-    make amy &&
-    yq '.workshops_current | map(del(.contact))' _data/all_amy.yml  > ../"$REPO_NAME"/_data/amy.json
+    make workshops &&
+    find _data -name '*_plain.json' -exec cp {} ../"$REPO_NAME"/_data/ \;
 
 cd ../"$REPO_NAME"  || exit
 
 git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout master
-git add _data/amy.json
+git add _data/*_plain.json
 git commit -m "[ci skip] update workshop data"
 git push deploy master
